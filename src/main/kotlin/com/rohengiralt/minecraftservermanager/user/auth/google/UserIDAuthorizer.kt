8e1869f94524ad820d7448522dbe5c60fe6f96db
battery-staple <batterystapledev@gmail.com>
2024-06-28 15:57:21 -0400

    Add logging when whitelist file is not accessible

--- src/main/kotlin/com/rohengiralt/minecraftservermanager/user/auth/google/UserIDAuthorizer.kt
+++ src/main/kotlin/com/rohengiralt/minecraftservermanager/user/auth/google/UserIDAuthorizer.kt
@@ -3,8 +3,10 @@ package com.rohengiralt.minecraftservermanager.user.auth.google
 import com.rohengiralt.minecraftservermanager.plugins.SecuritySpec
 import com.rohengiralt.minecraftservermanager.plugins.securityConfig
 import com.rohengiralt.minecraftservermanager.user.UserID
+import org.slf4j.LoggerFactory
 import kotlin.io.path.Path
 import kotlin.io.path.bufferedReader
+import kotlin.io.path.exists
 import kotlin.streams.asSequence
 
 interface UserIDAuthorizer {
@@ -13,6 +15,15 @@ interface UserIDAuthorizer {
 
 class WhitelistFileUserIDAuthorizer : UserIDAuthorizer {
     private val whitelistFile = Path(securityConfig[SecuritySpec.whitelistFile])
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+    init {
+        if (!whitelistFile.exists()) {
+            logger.error("Could not find whitelist file at `$whitelistFile`. Aborting.")
+            throw IllegalStateException("Could not find whitelist file")
+        }
+    }
+
     override fun isAuthorized(userId: UserID): Boolean = try {
         whitelistFile
             .bufferedReader()
@@ -20,6 +31,7 @@ class WhitelistFileUserIDAuthorizer : UserIDAuthorizer {
             .asSequence()
             .any { it.trim() == userId.idString }
     } catch (e: Throwable) {
-        false
+        logger.error("Could not read whitelist file.")
+        throw IllegalStateException("Could not read whitelist file")
     }
 }
