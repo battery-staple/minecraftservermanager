ae4fbca64e0c4754b50d971a555a543e13c8d565
battery-staple <batterystapledev@gmail.com>
2024-06-30 22:05:08 -0400

    Add documentation to SafeSQLState

--- src/main/kotlin/com/rohengiralt/minecraftservermanager/util/sql/SafeSQLState.kt
+++ src/main/kotlin/com/rohengiralt/minecraftservermanager/util/sql/SafeSQLState.kt
@@ -2,9 +2,16 @@ package com.rohengiralt.minecraftservermanager.util.sql
 
 import java.sql.SQLException
 
+/**
+ * A typesafe representation of this exception's [SQLException.SQLState]
+ */
 val SQLException.state: SQLState
     get() = SQLState.entries.firstOrNull { it.code == sqlState } ?: SQLState.OTHER
 
+/**
+ * Recognized SQL state responses.
+ * May be Postgres-specific.
+ */
 enum class SQLState(val code: String?) {
     UNIQUE_VIOLATION("23505"),
     OTHER(null);
